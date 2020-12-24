93f41eae1a583c968f25122ef1c6595384a2b3ce
Sophie Gu <sophieqgu@gmail.com>
2020-12-24 02:55:00 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,15 +9,14 @@ class PostsController < ApplicationController
   end
 
   def show
-    if params[:author_id]
-      @post = Author.find(params[:author_id]).posts.find(params[:id])
-    else
-      @post = Post.find(params[:id])
-    end
+    if params[:author_id] && !Author.exists?(params[:author_id])
+    redirect_to authors_path, alert: "Author not found."
+  else
+    @post = Post.new(author_id: params[:author_id])
   end
 
   def new
-    @post = Post.new
+    @post = Post.new(author_id: params[:author_id])
   end
 
   def create
@@ -33,12 +32,22 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    if params[:author_id]
+      author = Author.find_by(id: params[:author_id])
+      if author.nil?
+        redirect_to authors_path, alert: "Author not found."
+      else
+        @post = author.posts.find_by(id: params[:id])
+        redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+      end
+    else
+      @post = Post.find(params[:id])
+    end
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
